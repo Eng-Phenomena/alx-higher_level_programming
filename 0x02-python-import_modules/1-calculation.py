f554b563b4b89baf456b264605e9385ef89cab93
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-05 10:13:07 -0700

    2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     """printing arthimitic operations on 10 and 5"""
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add,sub,mul,div
 
     a = 10
     b = 5
