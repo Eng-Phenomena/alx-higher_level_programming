39ad745dcca695baafd82a231c4217428c016b8b
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-05 10:15:29 -0700

    2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """printing arthimitic operations on 10 and 5"""
+    """Prints some mathmatical opperations"""
     from calculator_1 import add,sub,mul,div
-
     a = 10
     b = 5
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}" .format(a, b, add(a,b)))
+    print("{} - {} = {}" .format(a, b, sub(a,b)))
+    print("{} * {} = {}" .format(a, b, mul(a,b)))
+    print("{} / {} = {}" .format(a, b, div(a,b)))
