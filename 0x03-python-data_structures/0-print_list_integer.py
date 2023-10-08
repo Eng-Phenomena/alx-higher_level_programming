dbb7f3022a0a2175c63159cfe2bad66f16cb42d3
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-08 11:03:01 -0700

    1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
-    """Printing all integer values in list"""
     for i in len(mylist):
-        print("{:d}".format(my_list(i)))
+        print("{:d}".format(my_list[i]))
