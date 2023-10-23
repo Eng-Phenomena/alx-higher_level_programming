f2d2653f17bc9929da0ff4e5514629321a4fd3b2
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-23 10:48:49 -0700

    5

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    arr = []
+    for i in  range(0, list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError):
+            print("division by 0")
+            div = 0
+        except (IndexError):
+            print("out of range")
+            div = 0
+        except (TypeError):
+            print("wrong type")
+            div = 0
+        finally:
+            arr.append(div)
+    return arr
