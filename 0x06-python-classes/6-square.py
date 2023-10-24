4894f5debb9476301d288742cde446de8f0af332
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-24 12:39:21 -0700

    7

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,6 +44,21 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """position getter"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """position setter"""
+        case = type(value[0]) != int or type(value[1]) != int
+        case2 = value[0] < 0 or value[1] < 0
+        if (case or case2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     def area(self):
         """method to claculate area"""
         return (self.__size ** 2)
