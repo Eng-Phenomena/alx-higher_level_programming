2500af9bab5a1757f6f349d80b6881865f02c211
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-24 11:34:02 -0700

    2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py done by ahmed
+"""square classs"""
+
+
+class Square:
+    """square class with private vars"""
+    def __init__(self, size):
+        """constructor of class"""
+        self.__size = size
