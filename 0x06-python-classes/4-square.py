623eab927b0e539255d66d800261d5a3db46a3ef
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-24 12:04:35 -0700

    5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# 3-square.py done by ahmed hossam
+"""class area method check"""
+
+
+class Square:
+    """area method"""
+    def __init__(self, size=0):
+        """constructor of class"""
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """get size value"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set size value"""
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """method to claculate area"""
+        return (self.__size ** 2)
