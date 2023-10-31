04f8a9cb4c16a8dece9808819046cafa035592b3
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-30 19:25:03 -0700

    f

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -36,7 +36,6 @@ def xout(board, row, col):
     All spots where non-attacking queens can no
     longer be played are X-ed out.
     """
-    # X out all forward spots
     for c in range(col + 1, len(board)):
         board[row][c] = "x"
     for c in range(col - 1, -1, -1):
