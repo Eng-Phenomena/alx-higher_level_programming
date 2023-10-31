aef218f1436a2f5016c3e0ac818a7070258a0cf3
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-30 19:12:37 -0700

    e

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,6 +8,19 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+     @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle"""
+        if (type(rect_1) != Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if (type(rect_2) != Rectangle_:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if (rect_1.area() >= rect_2.area()):
+            return rect_1
+        else:
+            return rect_2
+
+
     def __init__(self, width=0, height=0):
         """constructor of the recatngle"""
         self.height = height
@@ -67,15 +80,3 @@ class Rectangle:
         """prints a string at deletion"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """returns the biggest rectangle"""
-        if (type(rect_1) != Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if (type(rect_2) != Rectangle_:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if (rect_1.area() >= rect_2.area()):
-            return rect_1
-        else:
-            return rect_2
