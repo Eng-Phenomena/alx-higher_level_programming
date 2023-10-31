a5838d4903c01f51b5a47072df92587e19de4e30
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-30 18:55:25 -0700

    fixed

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -8,11 +8,33 @@ class Rectangle:
     count = 0
     symbol = "#"
 
+     @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle"""
+        if (type(rect_1) != Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if (type(rect_2) != Rectangle_:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if (rect_1.area() >= rect_2.area()):
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """new rectangel instance that is a square"""
+        return cls(size, size)
+
     def __init__(self, width=0, height=0):
         """constructor of the recatngle"""
         self.height = height
         self.width = width
         Rectangle.count += 1
+
+    def __del__(self):
+        """prints a string at deletion"""
+        print("Bye rectangle...")
+        Rectangle.count -= 1
 
     @property
     def width(self):
@@ -62,25 +84,3 @@ class Rectangle:
     def __repr__(self):
         """string representation of rectangle (reproduction)"""
         return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
-
-    def __del__(self):
-        """prints a string at deletion"""
-        print("Bye rectangle...")
-        Rectangle.count -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """returns the biggest rectangle"""
-        if (type(rect_1) != Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if (type(rect_2) != Rectangle_:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if (rect_1.area() >= rect_2.area()):
-            return rect_1
-        else:
-            return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """new rectangel instance that is a square"""
-        return cls(size, size)
