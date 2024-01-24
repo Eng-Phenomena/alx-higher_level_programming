1bbd1bf8ec55f969c07889fe44cccfe4675489a0
Eng-Phenomena <eng.phenomena@gmail.com>
2024-01-24 08:30:00 -0800

    fin

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 
-if (process.argv[2]){
-	console.log('Argument found');
-}
-else{
-	console.log('No argument');
+const { argv } = require('process');
+const argc = argv.length;
+
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
