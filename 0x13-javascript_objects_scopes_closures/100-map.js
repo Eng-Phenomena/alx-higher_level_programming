cb4fda224f9b087a8a2d62a4eecff74add0a4c1d
Eng-Phenomena <eng.phenomena@gmail.com>
2024-01-24 11:48:21 -0800

    4

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-console.log(list);
-console.log(list.map((x, i) => x * i));
+const li = require('./100-data').list;
+console.log(li);
+console.log(li.map((x, i) => x * i));
