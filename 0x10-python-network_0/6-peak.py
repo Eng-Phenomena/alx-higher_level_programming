3328326af3bc435d2619d647661c3462064729e6
Eng-Phenomena <eng.phenomena@gmail.com>
2024-01-25 06:58:03 -0800

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """Technical Interview problem [List of Numbers algorithim]."""
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
+    """Finds a peak in a list of unsorted ints."""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return (list_of_integers[0])
